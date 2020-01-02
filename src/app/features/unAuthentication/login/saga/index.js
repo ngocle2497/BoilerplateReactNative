512ab535aaa80666851a35526a10ff54bddb3a27
Ngoc LH <lhngoc2497@gmail.com>
2020-01-02 14:50:58 +0700

    delete js file

--- src/app/features/unAuthentication/login/saga/index.js
+++
@@ -1,6 +0,0 @@
-import {LOGIN} from '../redux/type';
-import * as Saga from './saga';
-import {takeLatest} from 'redux-saga/effects';
-export function* LoginSaga() {
-  yield takeLatest(LOGIN, Saga.onLogin);
-}
