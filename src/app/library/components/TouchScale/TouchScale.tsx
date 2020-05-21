aaeca3f2565574d38a55f2034d86b98ed8d7504f
HongNgoc <hngocl>
2020-05-21 14:36:19 +0700

    update touchable scale

--- src/app/library/components/TouchScale/TouchScale.tsx
+++ src/app/library/components/TouchScale/TouchScale.tsx
@@ -1,41 +1,41 @@
 import React, { memo } from 'react'
-import { PanGestureHandler, State } from 'react-native-gesture-handler';
-import { onGestureEvent, useValues, withTimingTransition, between } from 'react-native-redash';
-import Animated, { interpolate, eq, useCode, cond, and, call, block, onChange, set } from 'react-native-reanimated';
+import { PanGestureHandler, State, TapGestureHandler, LongPressGestureHandler, LongPressGestureHandlerGestureEvent } from 'react-native-gesture-handler';
+import { onGestureEvent, useValue, withTransition } from 'react-native-redash';
+import Animated, { interpolate, eq, cond, } from 'react-native-reanimated';
 import { TouchableScaleProps } from './Touch.props';
 import { equals } from 'ramda';
-
 const TouchableScaleComponent = (props: TouchableScaleProps) => {
-    const { children, minScale = 0.95, onPress, onLongPress, onPressIn, onPressOut } = props
-    const [state, translationY, translationX, x, y] = useValues([State.UNDETERMINED, 0, 0, 0, 0], [])
-    const duration = cond(eq(state, State.BEGAN), 300, 150)
-    const progress = withTimingTransition(eq(state, State.BEGAN), { duration: duration })
-    const _onGestureHandler = onGestureEvent({ state, translationY, translationX, x, y })
+    const { children, minScale = 0.9, onPress, onLongPress, onPressIn, onPressOut } = props;
+    const state = useValue(State.UNDETERMINED)
+    const duration = cond(eq(state, State.ACTIVE), 300, 150)
+    const progress = withTransition(eq(state, State.BEGAN), { duration: duration })
+    const _onGestureHandler = onGestureEvent({ state })
     const scale = interpolate(progress, {
         inputRange: [0, 1],
         outputRange: [1, minScale]
     })
-    useCode(() =>
-        onChange(scale, block([
-            cond(eq(scale, minScale), [
-                set(state, State.UNDETERMINED),
-                call([], ([]) => {
-                    onLongPress && onLongPress()
-                })
-            ]),
-            cond(and(eq(state, State.END), between(translationX, -5, 5), between(translationY, -5, 5)),
-                [set(state, State.UNDETERMINED), call([], ([]) => {
-                    onPressIn && onPressIn()
-                    onPressOut && onPressOut()
-                    onPress && onPress()
-                }
-                )])
-        ]))
-        , [])
+    const _onHandlerLongPress = (e: LongPressGestureHandlerGestureEvent) => {
+        if (e.nativeEvent.state === State.ACTIVE && onLongPress && typeof onLongPress === 'function') {
+            onLongPress()
+        }
+    }
+    const _onHandlerPress = (e: LongPressGestureHandlerGestureEvent) => {
+        if (e.nativeEvent.state === State.ACTIVE && onPress && typeof onPress === 'function') {
+            onPressIn && onPressIn()
+            onPress()
+            onPressOut && onPressOut()
+        }
+    }
     return (
         <PanGestureHandler {..._onGestureHandler}>
-            <Animated.View style={[{ transform: [{ scale }] }]}>
-                {children}
+            <Animated.View>
+                <LongPressGestureHandler maxDist={5} onHandlerStateChange={_onHandlerLongPress}>
+                    <TapGestureHandler onHandlerStateChange={_onHandlerPress}>
+                        <Animated.View style={[{ transform: [{ scale }] }]}>
+                            {children}
+                        </Animated.View>
+                    </TapGestureHandler>
+                </LongPressGestureHandler>
             </Animated.View>
         </PanGestureHandler>
     )
