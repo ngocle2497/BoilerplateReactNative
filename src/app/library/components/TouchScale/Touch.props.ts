eee594a83250341bd8237659d9c72358c5fa9291
HongNgoc <hngocl>
2020-05-27 13:11:43 +0700

    update touch scale

--- src/app/library/components/TouchScale/Touch.props.ts
+++ src/app/library/components/TouchScale/Touch.props.ts
@@ -1,4 +1,3 @@
-
 export interface TouchableScaleProps {
     children: React.ReactNode;
 
--- src/app/library/components/TouchScale/TouchScale.tsx
+++ src/app/library/components/TouchScale/TouchScale.tsx
@@ -1,43 +1,40 @@
-import React, { memo } from 'react'
-import { PanGestureHandler, State, TapGestureHandler, LongPressGestureHandler, LongPressGestureHandlerGestureEvent } from 'react-native-gesture-handler';
-import { onGestureEvent, useValue, withTransition } from 'react-native-redash';
-import Animated, { interpolate, eq, cond, } from 'react-native-reanimated';
+import React, { memo, useMemo, useCallback } from 'react'
+import { TouchableWithoutFeedback, Animated } from 'react-native';
 import { TouchableScaleProps } from './Touch.props';
 import { equals } from 'ramda';
+
 const TouchableScaleComponent = (props: TouchableScaleProps) => {
     const { children, minScale = 0.9, onPress, onLongPress, onPressIn, onPressOut } = props;
-    const state = useValue(State.UNDETERMINED)
-    const duration = cond(eq(state, State.ACTIVE), 300, 150)
-    const progress = withTransition(eq(state, State.BEGAN), { duration: duration })
-    const _onGestureHandler = onGestureEvent({ state })
-    const scale = interpolate(progress, {
-        inputRange: [0, 1],
-        outputRange: [1, minScale]
-    })
-    const _onHandlerLongPress = (e: LongPressGestureHandlerGestureEvent) => {
-        if (e.nativeEvent.state === State.ACTIVE && onLongPress && typeof onLongPress === 'function') {
+    const scale = useMemo(() => new Animated.Value(1), [])
+    const runAnimated = useCallback((value: number) => {
+        Animated.timing(scale, {
+            toValue: value,
+            duration: 150,
+            useNativeDriver: true
+        }).start()
+    }, [])
+    const _onPressIn = useCallback(() => {
+        runAnimated(minScale)
+        onPressIn && onPressIn()
+    }, [onPressIn])
+    const _onLongPress = useCallback(() => {
+        if (typeof onLongPress === 'function') {
+            runAnimated(1);
             onLongPress()
         }
-    }
-    const _onHandlerPress = (e: LongPressGestureHandlerGestureEvent) => {
-        if (e.nativeEvent.state === State.ACTIVE && onPress && typeof onPress === 'function') {
-            onPressIn && onPressIn()
-            onPress()
-            onPressOut && onPressOut()
-        }
-    }
+    }, [onLongPress])
+    const _onPressOut = useCallback(() => {
+        runAnimated(1)
+        onPressOut && onPressOut()
+    }, [onPressOut])
+    const _onPress = useCallback(() => { runAnimated(1); onPress && onPress() }, [onPress])
     return (
-        <PanGestureHandler {..._onGestureHandler}>
-            <Animated.View>
-                <LongPressGestureHandler maxDist={5} onHandlerStateChange={_onHandlerLongPress}>
-                    <TapGestureHandler onHandlerStateChange={_onHandlerPress}>
-                        <Animated.View style={[{ transform: [{ scale }] }]}>
-                            {children}
-                        </Animated.View>
-                    </TapGestureHandler>
-                </LongPressGestureHandler>
+        <TouchableWithoutFeedback onLongPress={_onLongPress} onPressIn={_onPressIn} onPressOut={_onPressOut} onPress={_onPress}>
+            <Animated.View style={[{ transform: [{ scale }] }]}>
+                {children}
             </Animated.View>
-        </PanGestureHandler>
+        </TouchableWithoutFeedback>
+
     )
 }
 export const TouchableScale = memo(TouchableScaleComponent, (prevProps, nextProps) => equals(prevProps, nextProps))
