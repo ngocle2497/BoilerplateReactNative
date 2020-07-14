5fb58f40bda8ec2eb825ac6b24559a24ed3037fd
Hồng Ngọc <43195241+hngocl@users.noreply.github.com>
2020-07-14 14:06:02 +0700

    Update index.ts

--- src/app/common/math/index.ts
+++ src/app/common/math/index.ts
@@ -1,9 +1,13 @@
-export const isNumber = (num: any) => {
+export const isNumber = (num: any): boolean => {
   return !isNaN(parseFloat(String(num)));
 };
-export const tryParseNumber = (num: any) => {
+export const tryParseNumber = (num: any): any => {
   if (isNumber(num)) {
     return parseFloat(String(num));
   }
   return num;
+};
+
+export const roundMaxFixed = (num: number, decimals: number): number => {
+  return Number(Math.round(Number(String(num + 'e' + decimals))) + 'e-' + decimals);
 };
