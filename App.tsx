b3ae5bf931fa0101c6740e785251e09670802d4a
HongNgoc <hngocl>
2020-08-27 18:21:17 +0700

    add I18nProvider

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import I18n from './src/app/library/utils/i18n/i18n';
 import { AppContainer } from './src/app/navigation/AppNavigation';
 import { Transitioning } from 'react-native-reanimated';
 import { _transitionApp, transition } from './src/app/transition/TransitionService';
+import { I18nextProvider } from 'react-i18next';
 
 console.disableYellowBox = true;
 const styles = StyleSheet.create({
@@ -18,11 +19,13 @@ export const MyApp = () => {
   return (
     <SafeAreaProvider>
       <Provider store={store}>
-        <Suspense fallback={<View />}>
-          <Transitioning.View style={styles.root} transition={transition} ref={_transitionApp}>
-            <AppContainer />
-          </Transitioning.View>
-        </Suspense>
+        <I18nextProvider i18n={I18n}>
+          <Suspense fallback={<View />}>
+            <Transitioning.View style={styles.root} transition={transition} ref={_transitionApp}>
+              <AppContainer />
+            </Transitioning.View>
+          </Suspense>
+        </I18nextProvider>
       </Provider>
     </SafeAreaProvider>
   );
