efa1656d7dc2f21b71971d70829a3015443c1b90
HongNgoc <hngocl>
2020-09-10 16:34:31 +0700

    change type

--- src/app/config/type.ts
+++ src/app/config/type.ts
@@ -1,4 +1,4 @@
-import {Theme} from '@react-navigation/native';
+import { Theme } from '@react-navigation/native';
 export interface ResponseBase<T = any> {
   code: number;
 
@@ -9,44 +9,82 @@ export interface ResponseBase<T = any> {
   status: boolean;
 }
 export interface Colors {
+
   primary: string;
+
   background: string;
+
   card: string;
+
   text: string;
+
   border: string;
+
   notification: string;
+
   error: string;
+
   info: string;
+
 }
 export interface FontSize {
+
   FONT_4: number;
+
   FONT_5: number;
+
   FONT_6: number;
+
   FONT_7: number;
+
   FONT_8: number;
+
   FONT_9: number;
+
   FONT_10: number;
+
   FONT_11: number;
+
   FONT_12: number;
+
   FONT_13: number;
+
   FONT_14: number;
+
   FONT_15: number;
+
   FONT_16: number;
+
   FONT_17: number;
+
   FONT_18: number;
+
   FONT_19: number;
+
   FONT_20: number;
+
   FONT_21: number;
+
   FONT_22: number;
+
   FONT_23: number;
+
   FONT_24: number;
+
   FONT_25: number;
+
   FONT_26: number;
+
   FONT_27: number;
+
   FONT_28: number;
+
   FONT_29: number;
+
   FONT_30: number;
+
   FONT_31: number;
+
   FONT_32: number;
 }
 export interface FontFamily {
@@ -64,4 +102,5 @@ export interface Spacing {
   huge: number;
   massive: number;
 }
-export interface AppTheme extends Theme {}
+
+export type AppTheme = Omit<Theme, 'colors'> & { colors: Partial<Colors> }
