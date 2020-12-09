982dff8b563a6794adf1f2e70fe5c1cdf2b28546
vmo <vmo@vmos-iMac.local>
2020-12-09 10:19:59 +0700

    update switch

--- src/app/library/components/Switch/Switch.props.ts
+++ src/app/library/components/Switch/Switch.props.ts
@@ -1,10 +1,24 @@
+import { ViewStyle, StyleProp } from 'react-native';
+
+type type = 'android' | 'ios'
+
 export interface SwitchProps {
+  /**
+   * Type of switch
+   * @default ios
+   */
+  type?: type;
   /**
    * Current state of switch
    * @default false
    */
   initialValue?: boolean;
 
+  /**
+   * Disable switch
+   * @default false
+   */
+  disable?: boolean;
   /**
    * Call back when switch press
    * @default undefined
--- src/app/library/components/Switch/Switch.tsx
+++ src/app/library/components/Switch/Switch.tsx
@@ -1,81 +1,11 @@
-import React, { memo, useCallback, useEffect, useState } from 'react';
-import {
-  TouchableWithoutFeedback,
-  StyleSheet,
-} from 'react-native';
-import { SwitchProps } from './Switch.props';
-import equals from 'react-fast-compare';
-import { interpolateColor, useTimingTransition } from '@animated';
-import Animated, {
-  interpolate,
-  Extrapolate,
-} from 'react-native-reanimated';
-import { onCheckType } from '@common';
+import React, { memo } from 'react'
+import isEqual from 'react-fast-compare'
+import { SwitchProps } from './Switch.props'
+import { Switch as SwitchIOS } from './Switch_IOS'
+import { Switch as SwitchAndroid } from './Swtich_Android'
 
-// dimensions
-const THUMB_SIZE = 30;
-const WIDTH = 56;
-const MARGIN = 2;
-const OFF_POSITION = -0.5;
-const ON_POSITION = WIDTH - THUMB_SIZE - MARGIN;
-const BORDER_RADIUS = (THUMB_SIZE * 3) / 4;
+const SwitchComponent = (props: SwitchProps) => {
+    return props.type === 'android' ? <SwitchAndroid {...props}/> : <SwitchIOS {...props}/>
+}
 
-// colors
-const ON_COLOR = '#008000';
-const OFF_COLOR = '#e6e6e6';
-const BORDER_OFF_COLOR = 'rgba(0, 0, 0, 0.1)';
-
-const styles = StyleSheet.create({
-  track: {
-    width: WIDTH,
-    height: THUMB_SIZE + MARGIN,
-    borderRadius: BORDER_RADIUS,
-    borderWidth: MARGIN / 2,
-  },
-  thumb: {
-    position: 'absolute',
-    width: THUMB_SIZE,
-    height: THUMB_SIZE,
-    borderColor: BORDER_OFF_COLOR,
-    borderRadius: THUMB_SIZE / 2,
-    backgroundColor: '#FFFFFF',
-    shadowColor: BORDER_OFF_COLOR,
-    shadowOffset: { width: 1, height: 2 },
-    shadowOpacity: 1,
-    shadowRadius: 2,
-    elevation: 2,
-  }
-});
-
-const SwitchComponent = ({
-  onToggle,
-  initialValue = false
-}: SwitchProps) => {
-  const [value, setValue] = useState<boolean>(initialValue)
-  const progress = useTimingTransition(value)
-  const translateX = interpolate(progress, {
-    inputRange: [0, 1],
-    outputRange: [OFF_POSITION, ON_POSITION],
-    extrapolate: Extrapolate.CLAMP
-  })
-  const backgroundColor = interpolateColor(progress, {
-    inputRange: [0, 1],
-    outputRange: [OFF_COLOR, ON_COLOR],
-  })
-  const _onToggle = useCallback(() => {
-    setValue(v => !v)
-  }, [])
-  useEffect(() => {
-    if (onToggle && onCheckType(onToggle, 'function')) {
-      onToggle(value)
-    }
-  }, [value])
-  return (
-    <TouchableWithoutFeedback onPress={_onToggle}>
-      <Animated.View style={[styles.track, { backgroundColor }]}>
-        <Animated.View style={[styles.thumb, { transform: [{ translateX }] }]} />
-      </Animated.View>
-    </TouchableWithoutFeedback>
-  )
-};
-export const Switch = memo(SwitchComponent, equals);
+export const Switch = memo(SwitchComponent, isEqual)
