3bbb7bf13a0cc1aa2e002130e0d44791010712d0
vmo <vmo@vmos-iMac.local>
2020-12-16 15:51:36 +0700

    add onDetectUrlInTextToArray function

--- src/app/common/string/index.ts
+++ src/app/common/string/index.ts
@@ -107,3 +107,11 @@ export const onHandleTagToArrayText = (
   });
   return arrText;
 };
+type TextUrl = {
+  isLink: boolean;
+  source: string;
+}
+export const onDetectUrlInTextToArray = (text: string): Array<TextUrl> => {
+  const detectUrls = /((?:[a-z]+:\/\/)?(?:(?:[a-z0-9\-]+\.)+(?:[a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(?::[0-9]{1,5})?(?:\/[a-z0-9_\-.~]+)*(?:\/(?:[a-z0-9_\-.]*)(?:\?[a-z0-9+_\-.%=&amp;]*)?)?(?:#[a-zA-Z0-9!$&'(?:)*+.=-_~:@/?]*)?)(?:\s+|$)/
+  return text.split(detectUrls).map(x => detectUrls.test(x) ? { isLink: true, source: x } : { isLink: false, source: x })
+}
