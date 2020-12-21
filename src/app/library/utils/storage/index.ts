267b6d9380b2141601eec9625caeefebdc178447
vmo <vmo@vmos-iMac.local>
2020-12-21 12:00:55 +0700

    instead sensitive-info to asynStorage

--- src/app/library/utils/storage/index.ts
+++ src/app/library/utils/storage/index.ts
@@ -1,13 +1,24 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import { Platform, StyleSheet } from 'react-native';
+import RNSInfo from 'react-native-sensitive-info';
+
+const appName = "APP_Name"
+const storeName = Platform.select({ ios: 'keyChain', android: 'sharePreferences' })
+const dateCreateApp = new Date(2020, 10, 10, 0, 0, 0, 0)
+const AppKey = String(dateCreateApp) + appName + storeName
+
+const optionRNSensitive: RNSInfo.RNSensitiveInfoOptions = {
+  sharedPreferencesName: AppKey,
+  keychainService: AppKey
+}
 
 /**
  * Loads a string from storage.
  *
  * @param key The key to fetch.
  */
-export async function loadString(key: string) {
+export async function loadString(key: string, option?: RNSInfo.RNSensitiveInfoOptions) {
   try {
-    return await AsyncStorage.getItem(key);
+    return await RNSInfo.getItem(key, StyleSheet.flatten([optionRNSensitive, option]));
   } catch {
     // not sure why this would fail... even reading the RN docs I'm unclear
     return null;
@@ -20,9 +31,9 @@ export async function loadString(key: string) {
  * @param key The key to fetch.
  * @param value The value to store.
  */
-export async function saveString(key: string, value: string) {
+export async function saveString(key: string, value: string, option?: RNSInfo.RNSensitiveInfoOptions) {
   try {
-    await AsyncStorage.setItem(key, value);
+    await RNSInfo.setItem(key, value, StyleSheet.flatten([optionRNSensitive, option]));
     return true;
   } catch {
     return false;
@@ -34,9 +45,9 @@ export async function saveString(key: string, value: string) {
  *
  * @param key The key to fetch.
  */
-export async function load(key: string) {
+export async function load(key: string, option?: RNSInfo.RNSensitiveInfoOptions) {
   try {
-    const almostThere = await AsyncStorage.getItem(key);
+    const almostThere = await RNSInfo.getItem(key, StyleSheet.flatten([optionRNSensitive, option]));
     return typeof almostThere === 'string' ? JSON.parse(almostThere) : null;
   } catch {
     return null;
@@ -49,9 +60,9 @@ export async function load(key: string) {
  * @param key The key to fetch.
  * @param value The value to store.
  */
-export async function save(key: string, value: any) {
+export async function save(key: string, value: any, option?: RNSInfo.RNSensitiveInfoOptions) {
   try {
-    await AsyncStorage.setItem(key, JSON.stringify(value));
+    await RNSInfo.setItem(key, JSON.stringify(value), StyleSheet.flatten([optionRNSensitive, option]));
     return true;
   } catch {
     return false;
@@ -63,17 +74,96 @@ export async function save(key: string, value: any) {
  *
  * @param key The key to kill.
  */
-export async function remove(key: string) {
+export async function remove(key: string, option?: RNSInfo.RNSensitiveInfoOptions) {
   try {
-    await AsyncStorage.removeItem(key);
-  } catch {}
+    await RNSInfo.deleteItem(key, StyleSheet.flatten([optionRNSensitive, option]));
+  } catch { }
 }
+const createSensitiveStorage = (paramOption: RNSInfo.RNSensitiveInfoOptions = {}) => {
+  const options = StyleSheet.flatten([optionRNSensitive, paramOption])
+  // react-native-sensitive-info returns different a different structure on iOS
+  // than it does on Android.
+  //
+  // iOS:
+  // [
+  //   [
+  //     { service: 'app', key: 'foo', value: 'bar' },
+  //     { service: 'app', key: 'baz', value: 'quux' }
+  //   ]
+  // ]
+  //
+  // Android:
+  // {
+  //   foo: 'bar',
+  //   baz: 'quux'
+  // }
+  //
+  // See https://github.com/mCodex/react-native-sensitive-info/issues/8
+  //
+  // `extractKeys` adapts for the different structure to return the list of
+  // keys.
+  const extractKeys = Platform.select({
+    ios: (items: any) => items[0].map((item: any) => item.key),
+    android: Object.keys
+  });
 
-/**
- * Burn it all to the ground.
- */
-export async function clear() {
-  try {
-    await AsyncStorage.clear();
-  } catch {}
+  const noop = (...args: any[]) => null;
+
+  return {
+    async getItem(key: string, callback = noop) {
+      try {
+        // getItem() returns `null` on Android and `undefined` on iOS;
+        // explicitly return `null` here as `undefined` causes an exception
+        // upstream.
+        let result: null | string | undefined = await RNSInfo.getItem(key, options);
+
+        if (typeof result === "undefined") {
+          result = null;
+        }
+
+        callback(null, result);
+
+        return result;
+      } catch (error) {
+        callback(error);
+        throw error;
+      }
+    },
+
+    async setItem(key: string, value: string, callback = noop) {
+      try {
+        await RNSInfo.setItem(key, value, options);
+        callback(null);
+      } catch (error) {
+        callback(error);
+        throw error;
+      }
+    },
+
+    async removeItem(key: string, callback = noop) {
+      try {
+        await RNSInfo.deleteItem(key, options);
+        callback(null);
+      } catch (error) {
+        callback(error);
+        throw error;
+      }
+    },
+
+    async getAllKeys(callback = noop) {
+      try {
+        const values = await RNSInfo.getAllItems(options);
+        const result = extractKeys(values);
+        callback(null, result);
+        return result;
+      } catch (error) {
+        callback(error);
+        throw error;
+      }
+    }
+  };
 }
+export const reduxPersistStorage = createSensitiveStorage({
+  keychainService: AppKey,
+  sharedPreferencesName: AppKey
+});
--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -3,7 +3,15 @@ import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 import { rootSaga } from './rootSagas';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-
+/**
+ * Use this instead storage of reduxPersist
+ * 
+ */
+// import { reduxPersistStorage } from '@utils'
+// const persistConfig = {
+//     ...
+//     storage: reduxPersistStorage
+//   }
 const devMode = __DEV__;
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [...getDefaultMiddleware({serializableCheck:false}), sagaMiddleware];
