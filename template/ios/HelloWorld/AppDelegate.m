39048d4c5c7886c0ebbfc5d3acbefefb4fdd8686
hngocl <lhngoc2497@gmail.com>
2021-10-04 21:40:31 +0700

    remove function delete keychain

--- template/ios/HelloWorld/AppDelegate.m
+++ template/ios/HelloWorld/AppDelegate.m
@@ -28,20 +28,7 @@ static void InitializeFlipper(UIApplication *application) {
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [MMKV initializeMMKV:nil];
-  if (![[NSUserDefaults standardUserDefaults] objectForKey:@"FirstRun"]) {
-      // Delete all keychain of this application
-      NSArray *secItemClasses = @[(__bridge id)kSecClassGenericPassword,
-                                  (__bridge id)kSecClassInternetPassword,
-                                  (__bridge id)kSecClassCertificate,
-                                  (__bridge id)kSecClassKey,
-                                  (__bridge id)kSecClassIdentity];
-      for (id secItemClass in secItemClasses) {
-        NSDictionary *spec = @{(__bridge id)kSecClass: secItemClass};
-        SecItemDelete((__bridge CFDictionaryRef)spec);
-      }
-      [[NSUserDefaults standardUserDefaults] setValue:@"1strun" forKey:@"FirstRun"];
-      [[NSUserDefaults standardUserDefaults] synchronize];
-    }
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
