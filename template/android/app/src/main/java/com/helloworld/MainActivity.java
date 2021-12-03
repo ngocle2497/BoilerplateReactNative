98406799d17cbce1159dbeb48ea7459927cdeb15
Hồng Ngọc <lhngoc2497@gmail.com>
2021-12-03 22:19:24 +0700

    update icon app

--- template/android/app/src/main/java/com/helloworld/MainActivity.java
+++ template/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -1,6 +1,5 @@
 package com.helloworld;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
@@ -14,10 +13,7 @@ public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int drawableId = (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
-                ? R.drawable.bootsplash_dark
-                : R.drawable.bootsplash_light; //Default light theme
-        RNBootSplash.init(drawableId, MainActivity.this);
+         RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
     }
 
     /**
