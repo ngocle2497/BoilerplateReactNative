82e31ce0828b5408a91f8ee7c7a75c74361d0d19
Mason Le <43195241+MasonLe2497@users.noreply.github.com>
2022-02-17 10:51:39 +0700

    config api with version

--- template/src/app/library/networking/api.ts
+++ template/src/app/library/networking/api.ts
@@ -1,4 +1,21 @@
-export const ApiConstants = {
+const API_VERSION = '/api/v1/';
+
+const ApiEndPoint = {
   LOGIN: '',
   REFRESH_TOKEN: '',
+} as const;
+
+const configApi = () => {
+  const apiOb: any = {} as any;
+  Object.keys(ApiEndPoint).forEach(x => {
+    const valueApi = ApiEndPoint[x as keyof typeof ApiEndPoint];
+    apiOb[x] = API_VERSION + valueApi;
+  });
+  return apiOb;
 };
+
+type ApiConstantsType<T> = {
+  [a in keyof T]: string;
+};
+
+export const ApiConstants = configApi() as ApiConstantsType<typeof ApiEndPoint>;
