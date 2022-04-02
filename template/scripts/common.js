2d8ed4830e32cc49ac701c55369c2d6cb3fcdafc
Mason Le <lhngoc2497@gmail.com>
2022-04-02 23:02:48 +0700

    change module export

--- template/scripts/common.js
+++ template/scripts/common.js
@@ -2,21 +2,23 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports.loadEnvFile = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path.join('./', process.argv[2]), 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      }
-      const envJson = data.split('\n').reduce((prev, curr) => {
-        const firstEqualSign = curr.indexOf('=');
-        const key = curr.slice(0, firstEqualSign);
-        const value = curr.slice(firstEqualSign + 1);
-        prev[key] = value;
-        return prev;
-      }, {});
-      resolve(envJson);
+module.exports = {
+  loadEnvFile: () => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path.join('./', process.argv[2]), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        const envJson = data.split('\n').reduce((prev, curr) => {
+          const firstEqualSign = curr.indexOf('=');
+          const key = curr.slice(0, firstEqualSign);
+          const value = curr.slice(firstEqualSign + 1);
+          prev[key] = value;
+          return prev;
+        }, {});
+        resolve(envJson);
+      });
     });
-  });
+  },
 };
