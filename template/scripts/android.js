b70dd9a7b91f316a8803b4ae2a02dda297769168
Mason Le <lhngoc2497@gmail.com>
2022-04-29 00:39:16 +0700

    uninstall app before install by adb/xcrun

--- template/scripts/android.js
+++ template/scripts/android.js
@@ -6,9 +6,18 @@ const { loadEnvFile } = require('./common');
 (async function () {
   const { argv, platform } = process;
   const envJson = await loadEnvFile();
+  // uninstall android app with adb
+  const devicesString = execSync('adb devices').toString().trim();
+  if (devicesString.split('\n').length > 1) {
+    try {
+      execSync(`adb uninstall ${envJson.BUNDLE_IDENTIFIER}`);
+    } catch {
+      console.log('Not have app');
+    }
+  }
   if (platform === 'darwin') {
     execSync(
-      `ENVFILE=${argv[2]} && cd android && ./gradlew clean -PdefaultEnvFile=${argv[2]} && cd .. && npx react-native run-android --variant=${argv[3]} --appId=${envJson.BUNDLE_IDENTIFIER}`,
+      `ENVFILE=${argv[2]} && cd android && ./gradlew clean '-PdefaultEnvFile=${argv[2]}' && cd .. && npx react-native run-android --variant=${argv[3]} --appId=${envJson.BUNDLE_IDENTIFIER}`,
       { stdio: 'inherit' },
     );
   } else if (platform === 'win32') {
--- template/scripts/ios.js
+++ template/scripts/ios.js
@@ -10,6 +10,15 @@ const { loadEnvFile } = require('./common');
   }
   const envJson = await loadEnvFile();
   const simulator = 'iPhone 11';
+  try {
+    // if simulator is not booted, it will throw an error
+    execSync(`xcrun simctl list devices | grep "${simulator}" | grep "Booted"`);
+  } catch {
+    execSync(`xcrun simctl boot "${simulator}"`);
+  }
+
+  // uninstall app using xcrun
+  execSync(`xcrun simctl uninstall booted "${envJson.BUNDLE_IDENTIFIER}"`);
   execSync(
     `cd ios && xcodebuild clean && cd .. && npx react-native run-ios --scheme ${envJson.APP_PLACEHOLDER_NAME}-${envJson.APP_ENV} --simulator="${simulator}"`,
     { stdio: 'inherit' },
