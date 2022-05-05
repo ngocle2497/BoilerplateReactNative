6a04d05b04eb1de58e34aa642985cf3a983fe603
Mason Le <lhngoc2497@gmail.com>
2022-05-05 22:55:30 +0700

    add useEventCallback hooks

--- template/src/app/common/hooks/index.ts
+++ template/src/app/common/hooks/index.ts
@@ -5,6 +5,7 @@ import React, {
   SetStateAction,
   useCallback,
   useEffect,
+  useLayoutEffect,
   useMemo,
   useRef,
   useState,
@@ -580,6 +581,39 @@ function useMessageYupTranslation(msg?: string) {
   }, [parsed, t, msg]);
 }
 
+/**
+ * @description Like 'useCallback' but with empty deps array.
+ * Don't use this hooks when you want to render something on React Tree.
+ * It will return previous value like usePrevious. first render will return undefined.
+ * @example
+ * This will render the previous value. don't use this:
+ * ```tsx 
+ * const total = useEventCallback(() => state1 + state2)
+ * <Text>{total()}</Text>
+ * ```
+ * 
+ * Use this:
+ * ```tsx
+ * const [msg,setMsg] = useState('');
+ * const sendMsg = useEventCallback(() => sendMsgToApi(msg));
+ * ```
+ */
+const useEventCallback = <Fn extends (...args: any[]) => ReturnType<Fn>>(
+  func: Fn,
+) => {
+  const callbackRef = useRef<(...args: Parameters<Fn>) => ReturnType<Fn>>();
+
+  const callbackMemoized = useCallback((...args: Parameters<Fn>) => {
+    return callbackRef.current?.(...args);
+  }, []);
+
+  useLayoutEffect(() => {
+    callbackRef.current = (...args) => func(...args);
+  });
+
+  return callbackMemoized;
+};
+
 export {
   useMessageYupTranslation,
   useIsMounted,
@@ -603,4 +637,5 @@ export {
   useMounted,
   useIsKeyboardShown,
   useDidMount,
+  useEventCallback
 };
