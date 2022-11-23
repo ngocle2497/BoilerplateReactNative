210da23de09eb3d4db521df077d54eea05708801
Mason Le <lhngoc2497@gmail.com>
2022-11-24 01:50:33 +0700

    remove clean android when build

--- template/scripts/android.js
+++ template/scripts/android.js
@@ -17,12 +17,12 @@ const { loadEnvFile } = require('./common');
   }
   if (platform === 'darwin') {
     execSync(
-      `ENVFILE=${argv[2]} && cd android && ./gradlew clean '-PdefaultEnvFile=${argv[2]}' && cd .. && npx react-native run-android --variant=${argv[3]} --appId=${envJson.BUNDLE_IDENTIFIER}`,
+      `npx react-native run-android --variant=${argv[3]} --appId=${envJson.BUNDLE_IDENTIFIER}`,
       { stdio: 'inherit' },
     );
   } else if (platform === 'win32') {
     execSync(
-      `SET ENVFILE=${argv[2]} && cd android && gradlew clean -PdefaultEnvFile=${argv[2]} && cd .. && npx react-native run-android --variant=${argv[3]} --appId=${envJson.BUNDLE_IDENTIFIER}`,
+      `npx react-native run-android --variant=${argv[3]} --appId=${envJson.BUNDLE_IDENTIFIER}`,
       { stdio: 'inherit', shell: 'cmd.exe' },
     );
   }
