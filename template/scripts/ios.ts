c9d0b3fffe13854b3d4a03f488e4b24aa2fd514a
Mason Le <lhngoc2497@gmail.com>
2023-03-07 11:36:49 +0700

    update command ios

--- template/scripts/ios.ts
+++ template/scripts/ios.ts
@@ -2,6 +2,30 @@ import { execSync } from 'child_process';
 
 import { loadEnvFile, setupEnv } from './common';
 
+const bootDevice = (deviceName: string) => {
+  /**
+   * iPhone 8 (2292DF56-3D4D-4328-90CF-C804E8A1A7F5) (Shutdown)
+   * iPhone 11 (7590DD1E-D039-4545-ADB1-586F2471D873) (Booted)
+   * iPhone 11 Pro (24C004AB-A617-4168-B0F2-37FD9223A1C0) (Booted)
+   */
+  try {
+    // if simulator is not booted, it will throw an error
+    execSync(
+      `xcrun simctl list devices | grep "${deviceName}" | grep "Booted"`,
+    );
+  } catch {
+    execSync(`xcrun simctl boot "${deviceName}"`);
+  }
+  return execSync(
+    `xcrun simctl list devices | grep "${deviceName}" | grep "Booted" | grep -E -o -i "([0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12})"`,
+  ).toString();
+};
+
+const uninstallOldApp = (bundleId: string) => {
+  // uninstall app using xcrun
+  execSync(`xcrun simctl uninstall booted "${bundleId}"`);
+};
+
 const run = (props: { platform: NodeJS.Platform; envPath: string }) => {
   if (props.platform !== 'darwin') {
     console.log('This script is only for macOS');
@@ -10,17 +34,8 @@ const run = (props: { platform: NodeJS.Platform; envPath: string }) => {
   const envJson = loadEnvFile(props.envPath);
   setupEnv(props.envPath, envJson);
   const simulator = 'iPhone 11';
-  try {
-    // if simulator is not booted, it will throw an error
-    execSync(`xcrun simctl list devices | grep "${simulator}" | grep "Booted"`);
-  } catch {
-    execSync(`xcrun simctl boot "${simulator}"`);
-  }
-  const udid = execSync(
-    `xcrun simctl list devices | grep "${simulator}" | grep "Booted" | grep -E -o -i "([0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12})"`,
-  ).toString();
-  // uninstall app using xcrun
-  execSync(`xcrun simctl uninstall booted "${envJson.BUNDLE_IDENTIFIER}"`);
+  const udid = bootDevice(simulator);
+  uninstallOldApp(envJson.BUNDLE_IDENTIFIER);
   execSync(
     `npx react-native run-ios --scheme ${envJson.APP_PLACEHOLDER_NAME}-${envJson.APP_ENV} --udid=${udid}`,
     { stdio: 'inherit' },
@@ -40,30 +55,7 @@ const pushNotification = ({
   }
   const envJson = loadEnvFile(envPath);
   const simulator = 'iPhone 11';
-  try {
-    // if simulator is not booted, it will throw an error
-    execSync(`xcrun simctl list devices | grep "${simulator}" | grep "Booted"`);
-  } catch {
-    execSync(`xcrun simctl boot "${simulator}"`);
-  }
-  /**
-   * iPhone 8 (2292DF56-3D4D-4328-90CF-C804E8A1A7F5) (Shutdown)
-   * iPhone 11 (7590DD1E-D039-4545-ADB1-586F2471D873) (Booted)
-   * iPhone 11 Pro (24C004AB-A617-4168-B0F2-37FD9223A1C0) (Booted)
-   */
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const deviceId = execSync("xcrun simctl list | egrep '(Booted)' ")
-    .toString()
-    .trim()
-    .split('\n')
-    .find(x => x.includes(simulator))
-    .replace('(Booted)', '')
-    .replace(simulator, '')
-    .trim()
-    .replace('(', '')
-    .replace(')', '');
-
+  const deviceId = bootDevice(simulator);
   execSync(
     `xcrun simctl push ${deviceId} ${envJson.BUNDLE_IDENTIFIER} notification-ios-config.apns`,
     { stdio: 'inherit' },
