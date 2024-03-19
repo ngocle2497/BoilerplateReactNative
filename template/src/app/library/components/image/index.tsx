9f2aa7cbba15261016fa7c6148e1542c162fe8d8
Mason Le <43195241+masonle2x2@users.noreply.github.com>
2024-03-19 18:17:06 +0700

    feat(all): release new version

--- template/src/app/library/components/image/index.tsx
+++ template/src/app/library/components/image/index.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
 
-import { Image as ExpoImage } from 'expo-image';
+import { Image as ExpoImage } from "expo-image";
 
-import { ImageProps } from './type';
+import { ImageProps } from "./type";
 
 export const Image = ({ ...rest }: ImageProps) => {
   // render
   return (
     <ExpoImage
-      transition={{ duration: 600, effect: 'cross-dissolve' }}
-      placeholder={{ thumbhash: '3PcNNYSFeXh/k0oGLQaSVsN0BVhn2oq2Z5SQUQcZ' }}
+      transition={{ duration: 300, effect: "cross-dissolve" }}
+      placeholderContentFit="cover"
+      placeholder={{
+        thumbhash: "3PcNNYSFeXh/k0oGLQaSVsN0BVhn2oq2Z5SQUQcZ",
+        cacheKey:
+          rest.recyclingKey ||
+          (typeof rest.source === "string" ? rest.source : undefined),
+      }}
       {...rest}
-      style={[StyleSheet.absoluteFillObject]}
+      style={StyleSheet.absoluteFillObject}
     />
   );
 };
