93c4e0f5646fb609a56d6ba554a546aa8d02f4ce
Ngọc Lê <43195241+masonle2x2@users.noreply.github.com>
2024-05-02 23:44:15 +0700

    add yarn 3 for rn 0.74

--- template/scripts/setup.js
+++ template/scripts/setup.js
@@ -4,47 +4,57 @@ const { execSync } = require('child_process');
 const { getAndroidHome, getRubyVersion } = require('./common');
 
 (function () {
-  execSync('yarn patch-package', { stdio: 'inherit' });
+  try {
+    execSync('npx lefthook install', { stdio: 'inherit' });
 
-  if (getAndroidHome() !== '') {
-    execSync(`echo "sdk.dir=${getAndroidHome()}" > android/local.properties`, {
-      stdio: 'inherit',
-    });
-  }
+    execSync('yarn patch-package', { stdio: 'inherit' });
 
-  if (process.platform === 'darwin') {
-    execSync('cd ios && touch tmp.xcconfig');
-
-    if (getRubyVersion() < 276) {
-      console.log(
-        '                  🧐🧐🧐🧐🧐 Installing CocoaPods dependencies!! 🧐🧐🧐🧐🧐',
+    if (getAndroidHome() !== '') {
+      execSync(
+        `echo "sdk.dir=${getAndroidHome()}" > android/local.properties`,
+        {
+          stdio: 'inherit',
+        },
       );
-
-      execSync('pod install --project-directory=ios', {
-        stdio: 'inherit',
-      });
-
-      console.log('                      ✨✨✨✨✨ Pod done!!! ✨✨✨✨✨');
-    } else {
-      console.log(
-        '                  🧐🧐🧐🧐🧐 Installing Bundle dependencies!! 🧐🧐🧐🧐🧐',
-      );
-
-      execSync('bundle install', {
-        stdio: 'inherit',
-      });
-
-      console.log('bundle install Done!!✨✨✨✨✨');
-
-      console.log(
-        '                  🧐🧐🧐🧐🧐 Installing CocoaPods dependencies!! 🧐🧐🧐🧐🧐',
-      );
-
-      execSync('bundle exec pod install --project-directory=ios', {
-        stdio: 'inherit',
-      });
-
-      console.log('                      ✨✨✨✨✨ Pod done!!! ✨✨✨✨✨');
     }
-  }
+
+    if (process.platform === 'darwin') {
+      execSync('cd ios && touch tmp.xcconfig');
+
+      if (getRubyVersion() < 276) {
+        console.log(
+          '                  🧐🧐🧐🧐🧐 Installing CocoaPods dependencies!! 🧐🧐🧐🧐🧐',
+        );
+
+        execSync('pod install --project-directory=ios', {
+          stdio: 'inherit',
+        });
+
+        console.log('                      ✨✨✨✨✨ Pod done!!! ✨✨✨✨✨');
+      } else {
+        console.log(
+          '                  🧐🧐🧐🧐🧐 Installing Bundle dependencies!! 🧐🧐🧐🧐🧐',
+        );
+
+        execSync('bundle install', {
+          stdio: 'inherit',
+        });
+
+        console.log('bundle install Done!!✨✨✨✨✨');
+
+        console.log(
+          '                  🧐🧐🧐🧐🧐 Installing CocoaPods dependencies!! 🧐🧐🧐🧐🧐',
+        );
+
+        execSync(
+          'bundle exec pod install --project-directory=ios --repo-update',
+          {
+            stdio: 'inherit',
+          },
+        );
+
+        console.log('                      ✨✨✨✨✨ Pod done!!! ✨✨✨✨✨');
+      }
+    }
+  } catch {}
 })();
