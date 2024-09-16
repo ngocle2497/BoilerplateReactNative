3856b3700ca7b9e3fd853aac818f35cf78dbbfce
Ngoc Le <43195241+ngocle2497@users.noreply.github.com>
2024-09-16 20:42:34 +0700

    update version i18n

--- template/src/app/library/utils/i18n/i18n.ts
+++ template/src/app/library/utils/i18n/i18n.ts
@@ -2,51 +2,20 @@
 import { initReactI18next } from 'react-i18next';
 
 import { DEFAULT_FALLBACK_LNG_I18n } from '@env';
-import i18n, { LanguageDetectorAsyncModule, Resource } from 'i18next';
+import i18n from 'i18next';
 
 import { resources } from './locales';
 
-const languageDetector: LanguageDetectorAsyncModule = {
-  // flags below detection to be async
-  async: true,
-
-  cacheUserLanguage: () => {},
-  detect: (callback: (lng: string | readonly string[] | undefined) => void) => {
-    callback(DEFAULT_FALLBACK_LNG_I18n);
-  },
-  init: () => {},
-  type: 'languageDetector',
-};
-
-export const initOptionsI18n = (source: Resource) => {
-  return {
-    debug: false,
-
-    defaultNS: 'common',
-
+(() => {
+  i18n.use(initReactI18next).init({
+    compatibilityJSON: 'v3',
     fallbackLng: DEFAULT_FALLBACK_LNG_I18n,
-
-    // cache: {
-    //   enabled: true
-    // },
     interpolation: {
-      // not needed for react as it does escape per default to prevent xss!
       escapeValue: false,
     },
-
-    // have a common namespace used around the full app
-    ns: ['common'],
-
-    resources: source,
-  };
-};
-
-/**
- * Config i18n for app
- */
-i18n
-  .use(languageDetector)
-  .use(initReactI18next)
-  .init(initOptionsI18n(resources));
+    lng: DEFAULT_FALLBACK_LNG_I18n,
+    resources,
+  });
+})();
 
 export default i18n;
